1e517b2ac5f143278b1f259157c034afbfb5848f
Renata <73183182+Remonteiro99@users.noreply.github.com>
2022-10-21 23:01:51 -0300

    Create mvc.py

---
+++ mvc.py
@@ -0,0 +1,43 @@
+import os
+from flask import Flask, render_template, request
+from flaskext.mysql import MySQL
+
+mysql = MySQL()
+app = Flask(__name__)
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'mudar123'
+app.config['MYSQL_DATABASE_DB'] = 'alunos'
+app.config['MYSQL_DATABASE_HOST'] = '172.17.0.2'
+mysql.init_app(app)
+
+@app.route('/')
+def main():
+    return render_template('mvc.html')
+
+@app.route('/gravar', methods=['POST','GET'])
+def gravar():
+  nome = request.form['nome']
+  cpf = request.form['cpf']
+  endereco = request.form['endereco']
+  if nome and email and senha:
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute('insert into tbl_user (user_name, user_cpf, user_endereco) VALUES (%s, %s, %s)', (nome, cpf, endereco))
+    conn.commit()
+  return render_template('mvc.html')
+
+
+@app.route('/listar', methods=['POST','GET'])
+def listar():
+  conn = mysql.connect()
+  cursor = conn.cursor() #abre uma seção dentro da conexão
+  cursor.execute('select user_name, user_cpf, user_endereco alunos_ac1')
+  data = cursor.fetchall() #recuperar registros
+  conn.commit()
+  return render_template('lista.html', datas=data)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
